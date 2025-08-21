f86c02468e56326f97b6f999405a7a07f1666199
nusp09 <oscar.overall@protonmail.com>
2025-08-21 20:35:43 +0100

    do i got that shit on
    it worked !!!

--- early_pybricks_tests/nvim_compatability_test.py
+++ early_pybricks_tests/nvim_compatability_test.py
@@ -10,3 +10,12 @@ example_motor = Motor(Port.A)
 
 # Make the motor run clockwise at 500 degrees per second.
 example_motor.run(500)
+
+# Wait for three seconds.
+wait(3000)
+
+# Make the motor run counterclockwise at 500 degrees per second.
+example_motor.run(-500)
+
+# Wait for three seconds.
+wait(3000)
