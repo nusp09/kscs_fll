191ee39d2666b9595d0dcaa3d7abb6c04b03cf17
oscar overall <oscar.overall@protonmail.com>
2025-10-02 21:07:44 +0100

    your mom

---
+++ early_pybricks_tests/menu.py
@@ -0,0 +1,34 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop, Icon, Axis
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch, Matrix, run_task, multitask
+# all angles are relative
+motor_limits = [1000, 2000, 200]
+hub = PrimeHub()
+hub = PrimeHub(top_side=Axis.Z, front_side=Axis.X)
+hub.display.orientation(up=Side.LEFT)
+hub.speaker.volume(100)
+
+driveL = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+driveR = Motor(Port.B)
+AttachmentL = Motor(Port.C)
+AttachmentR = Motor(Port.D)
+SensorL = ColorSensor(Port.E)
+SensorR = ColorSensor(Port.F)
+drive = DriveBase(driveL, driveR, wheel_diameter=62.4, axle_track=111)
+drive.use_gyro(True)
+charger_status = {0:'not charging', 1:'charging', 2:'done charging', 3:'issue charging'}
+battery_down = Matrix([
+    [0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    ]
+
+        )*100
+hub.display.icon(battery_down)
+while True:
+    print(charger_status[hub.charger.status()])
+
