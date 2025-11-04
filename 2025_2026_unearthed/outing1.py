09a82ea88742a483036cc06a676e4ec8cd8bebdc
oscar o <oscar.overall@protonmail.com>
2025-11-04 16:59:16 +0000

    Add initial robot control program in outing1.py
    Implement basic robot movement and control logic.

---
+++ 2025_2026_unearthed/outing1.py
@@ -0,0 +1,32 @@
+#start at the first line of the program
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop, Icon, Axis
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch, Matrix, run_task, multitask
+# all angles are relative
+motor_limits = [1000, 2000, 200]
+hub = PrimeHub()
+hub = PrimeHub(top_side=Axis.Z, front_side=Axis.X)
+hub.display.orientation(up=Side.LEFT)
+hub.speaker.volume(100)
+
+driveL = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+driveR = Motor(Port.B)
+AttachmentL = Motor(Port.C)
+AttachmentR = Motor(Port.D)
+SensorL = ColorSensor(Port.E)
+SensorR = ColorSensor(Port.F)
+base = DriveBase(driveL, driveR, wheel_diameter=62.4, axle_track=111)
+base.use_gyro(True)
+#######
+base.drive(-1000,0)
+wait(100)
+base.straight(750)
+base.turn(45)
+
+AttachmentL.run_angle(500, 20)
+AttachmentL.run_angle(500, -20)
+base.straight(100)
+while True:
+    base.turn(-45)
