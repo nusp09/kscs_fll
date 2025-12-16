9c6198a10a79258f30e19eef1a8655447756a3e3
oscar o <oscar.overall@protonmail.com>
2025-12-16 16:12:33 +0000

    Add initial implementation of robot control in pensi.py

---
+++ 2025_2026_unearthed/pensi.py
@@ -0,0 +1,31 @@
+#start at the first line of the program
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop, Icon, Axis
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch, Matrix, run_task, multitask
+# all angles are relative
+motor_limits = [1000, 2000, 200]
+hub = PrimeHub()
+hub = PrimeHub(top_side=Axis.Z, front_side=Axis.X)
+hub.display.orientation(up=Side.LEFT)
+hub.speaker.volume(100)
+
+driveL = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+driveR = Motor(Port.B)
+AttachmentL = Motor(Port.C)
+AttachmentR = Motor(Port.D)
+SensorL = ColorSensor(Port.E)
+SensorR = ColorSensor(Port.F)
+base = DriveBase(driveL, driveR, wheel_diameter=62.4, axle_track=111)
+base.use_gyro(True)
+#######
+base.straight(570)
+AttachmentR.run_angle(500, -270)
+base.straight(-75)
+AttachmentR.run_angle(500, 270)
+base.turn(-90)
+base.straight(120)
+base.turn(90)
+base.straight(270)
+base.straight(-550)
