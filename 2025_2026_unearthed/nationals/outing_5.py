93d16f5868b174a5aa536d65111c66da151ba4c0
oscar overall <oscar.overall@protonmail.com>
2026-04-12 21:24:20 +0100

    outing 5
    90 pts in 17 seconds not bad gives us extra 2s per outing for
    changeovers

--- 2025_2026_unearthed/nationals/outing_5.py
+++ 2025_2026_unearthed/nationals/outing_5.py
@@ -20,3 +20,37 @@ SensorR = ColorSensor(Port.F)
 base = DriveBase(driveL, driveR, wheel_diameter=62.4, axle_track=111)
 base.use_gyro(True)
 #####
+async def front_rack(direction):
+    if direction == 'left':
+        await AttachmentL.run_angle(700,400)
+    if direction == 'right':
+        await AttachmentL.run_angle(700,-400)
+    if direction == 'right center':
+        await AttachmentL.run_angle(700,-250)
+    if direction == 'left center':
+        await AttachmentL.run_angle(700,250)
+
+async def arm(direction):
+    if direction == 'up':
+        await AttachmentR.run_angle(150,-180)
+    if direction == 'down':
+        await AttachmentR.run_angle(100,200)
+
+
+
+async def main():
+    await base.straight(800)
+    await front_rack('left')
+    await front_rack('right')
+    await base.straight(-50)
+    await multitask(base.turn(-90),front_rack('left center'))
+    await multitask(base.straight(-230),arm('down'))
+    await base.straight(-80)
+    await arm('up')
+    await base.straight(300)
+    await base.turn(-90)
+    await multitask(base.straight(700),front_rack('left'))
+
+    
+run_task(main())
+
