da240d8b7f005923d5be8ce4d5a1cab276113b6d
oscar overall <oscar.overall@protonmail.com>
2026-04-23 22:21:53 +0100

    add comments

--- 2025_2026_unearthed/nationals/waltah.py
+++ 2025_2026_unearthed/nationals/waltah.py
@@ -23,75 +23,77 @@ base.use_gyro(True)
 selected = hub_menu("1", "2", "3", "4", "5", "6")
 
 def init():
+    #use this to reduce motor backlash and gyro issues
     base.reset()
+    base.straight(-15)
 def outing1():
     base.straight(700)
     base.straight(-120)
     base.straight(50)
-    AttachmentL.run_angle(300,270)
+    AttachmentL.run_angle(300,270)#picks up brush after swiping it
     base.turn(45)
     base.straight(175)
     base.turn(-90)
     base.straight(180)
-    AttachmentR.run_angle(300,60)
+    AttachmentR.run_angle(300,60)#picks up topsoil
     base.straight(-50)
-    base.straight(60)
+    base.straight(60)#push forwards again to ensure all soil is uncovered
     base.straight(-120)
     base.turn(45)
     base.straight(-800)
     AttachmentL.run_angle(300,-270)
 def outing2():
-    right_angle = 360*11
+    right_angle = 360*11#set how to do a 90 degree turn with the screw gear
     base.straight(750)
-    AttachmentR.run_angle(1000,-right_angle)
-    AttachmentL.run_angle(1000,-right_angle*1.1)
+    AttachmentR.run_angle(1000,-right_angle)#puts arm down
+    AttachmentL.run_angle(1000,-right_angle*1.1)#turns the end gears
     AttachmentR.run_angle(1000,right_angle)
     base.straight(-720)
 def outing3():
+    #first introduction to asynchronous functions: an async function allows proscesses to be run inline with each other such as putting the arm down and moving forwards or putting it back up on a long straight section it alows us to run multiple motors at once with the main use for driving and moving motors
     async def move_arm():
-        await AttachmentR.run_angle(500,-180)
+        await AttachmentR.run_angle(500,-180)#puts the arm up
     async def outing_3():
         await base.straight(830)
         await base.turn(90)
-        await base.straight(900)
+        await base.straight(900)#this is where it pushes over the cart
         await base.turn(45)
         base.straight(150)
         await wait(200)
-        await AttachmentR.run_angle(500,180)
+        await AttachmentR.run_angle(500,180)#completes raise the roof and tip the scale
         await base.straight(-150)
         await base.turn(-45)
-        await multitask(base.straight(360),move_arm())
+        await multitask(base.straight(360),move_arm())#pulls attachment arm in whilst moving to avoid excess time loss
         await base.turn(70)
         await base.straight(740)
     run_task(outing_3())
 def outing4():
     base.straight(470)
+    #we use a for loop here to avoid repeating code unnessicarily
     for i in range(3):
-        AttachmentR.run_angle(500,-180,Stop.HOLD, False)
+        AttachmentR.run_angle(500,-180,Stop.HOLD, False)#neat little trick to move a motor for an ammount of time instead of an angle is to set the wait parameter to False
         wait(500)
-        AttachmentR.run_angle(200,140)
+        AttachmentR.run_angle(200,140)#empties silo
     base.straight(-170)
     AttachmentR.run_angle(250,-180,Stop.HOLD, False)
     wait(500)
-    base.turn(-55)
+    base.turn(-55)#completes market stall
     base.straight(120)
     base.turn(-45)
     AttachmentR.run_angle(250,180)
     base.straight(200)
     base.turn(25)
     AttachmentR.run_angle(250,-180)
-    base.turn(-9)
-    base.turn(-9)
-    base.turn(-9)
-    base.turn(-9)
-    base.turn(-9)
+    for i in range(5):
+        base.turn(-9)
     base.straight(-10)
     base.turn(-9)
     base.turn(-9)
-    base.turn(50)
+    base.turn(50)#pulls the pan out
     base.straight(-450)
     base.turn(-90)
 def outing5():
+    #our best use of the async function is this outing we used a function called frint rack and arm to control the two attachments and a main proscess to call these functions from
     async def front_rack(direction):
         if direction == 'left':
             await AttachmentL.run_angle(700,400)
@@ -109,15 +111,15 @@ def outing5():
             await AttachmentR.run_angle(100,200)
     
     
-    
+
     async def outing_5():
         await base.straight(800)
         await front_rack('left')
-        await front_rack('right')
+        await front_rack('right')#completes the who lived here 
         await base.straight(-50)
         await multitask(base.turn(-90),front_rack('left center'))
         await multitask(base.straight(-230),arm('down'))
-        await base.straight(-80)
+        await base.straight(-80)#picks up the heavy lifting
         await arm('up')
         await base.straight(300)
         await base.turn(-90)
@@ -127,7 +129,7 @@ def outing6():
     base.straight(680)
     base.turn(25)
     base.straight(400)
-    AttachmentR.run_angle(500,320)
+    AttachmentR.run_angle(500,320)#completes statue rebuilt
     base.straight(-200)
     base.turn(65)
     base.straight(500)
@@ -135,22 +137,29 @@ def outing6():
     base.straight(380)
     base.turn(-90)
     base.straight(40)
-    AttachmentL.run_angle(500,200)
+    AttachmentL.run_angle(500,200)#dumps all of the thingies in the forum
     AttachmentL.run_angle(500,-200)
     AttachmentL.run_angle(500,200)
     base.straight(-50)
     base.turn(-90)
-    base.straight(-180)
+    base.straight(-180)#finishes with the sand castle in the spot 
 ######
+#menu to select the outings
 if selected == "1":
+    init()
     outing1()
 elif selected == "2":
+    base.reset()
     outing2()
 elif selected == "3":
+    init()
     outing3()
 elif selected == "4":
+    init()
     outing4()
 elif selected == "5":
+    init()
     outing5()
 elif selected == "6":
+    base.reset()
     outing6()
